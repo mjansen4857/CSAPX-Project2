750c934b39b1b313b5defb738be3b9cadd33c6dd
Michael Jansen <mjansen4857@gmail.com>
2018-12-05 17:40:25 -0500

    Fixed tooltip

--- src/place/client/gui/PlaceGUI.java
+++ src/place/client/gui/PlaceGUI.java
@@ -1,10 +1,12 @@
 package place.client.gui;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -15,6 +17,8 @@ import place.PlaceException;
 import place.PlaceTile;
 import place.client.model.ClientModel;
 import place.client.network.NetworkClient;
+
+import java.awt.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -30,6 +34,7 @@ public class PlaceGUI extends Application implements Observer {
     private boolean firstUpdate = true;
     private Canvas canvas;
     private static final double SIZE = 600;
+    private Tooltip tp;
 
     public void init(){
         List< String > args = super.getParameters().getRaw();
@@ -54,11 +59,17 @@ public class PlaceGUI extends Application implements Observer {
         this.model.addObserver( this );
     }
 
+    private void updateTooltip(String text, double x, double y){
+        tp.setText(text);
+        tp.show(mainPane, x, y);
+    }
+
     @Override
     public synchronized void start(Stage mainStage) {
         while(!serverConn.isLoaded()) {}
         mainPane = new BorderPane();
         mainStage.setTitle("Place: " + serverConn.getUsername());
+        tp = new Tooltip();
 
         this.canvas = new Canvas(SIZE, SIZE);
         canvas.setOnMouseClicked((event) -> {
@@ -69,18 +80,33 @@ public class PlaceGUI extends Application implements Observer {
                 serverConn.sendMove(row, col, color);
             }
         });
-        Tooltip tp = new Tooltip();
-        canvas.setOnMouseMoved((event) -> {
-            double size = SIZE/model.getDim();
-            int row = (int) (event.getY()/size);
-            int col = (int) (event.getX()/size);
-            PlaceTile tile = model.getTile(row, col);
-            tp.setText("(" + row + "," + col + ")\n" +
-                    tile.getOwner() + "\n" +
-                    new SimpleDateFormat("MM/dd/yy HH:mm:ss").format(new Date(tile.getTime())) + "\n" +
-                    tile.getColor().getName());
-            tp.show(canvas, event.getScreenX() + 10, event.getScreenY() + 10);
-        });
+        
+        new Thread(() -> {
+            double size = SIZE / model.getDim();
+            while (serverConn.goodToGo()) {
+                Point p = MouseInfo.getPointerInfo().getLocation();
+                if (p.getX() >= mainStage.getX() + 10 && p.getX() <= mainStage.getX() + mainStage.getWidth() - 10) {
+                    if (p.getY() >= mainStage.getY() + 30 && p.getY() <= mainStage.getY() + mainStage.getHeight() - 40) {
+                        int row = (int) ((p.getY() - mainStage.getY() - 28) / size);
+                        int col = (int) ((p.getX() - mainStage.getX() - 6) / size);
+                        PlaceTile tile = model.getTile(row, col);
+                        Platform.runLater(() -> updateTooltip("Pos: (" + row + "," + col + ")\n" +
+                                "Owner: " + tile.getOwner() + "\n" +
+                                "Time: " + new SimpleDateFormat("MM/dd/yy HH:mm:ss").format(new Date(tile.getTime())) + "\n" +
+                                "Color: " + tile.getColor().getName(), p.getX() + 10, p.getY() + 10));
+                    } else {
+                        Platform.runLater(() -> tp.hide());
+                    }
+                } else {
+                    Platform.runLater(() -> tp.hide());
+                }
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
         drawBoard();
 
         HBox bottom = new HBox();
