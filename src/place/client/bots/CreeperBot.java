2d039ac2d13b17a911e93b67aff11f0a18509ca9
Tyson Levy <tjl3553@cs.rit.edu>
2018-12-05 17:59:08 -0500

    creeperbot works

--- src/place/client/bots/CreeperBot.java
+++ src/place/client/bots/CreeperBot.java
@@ -18,6 +18,33 @@ public class CreeperBot extends ConsoleApplication implements Observer {
 
     public void init() {
 
+        for(int i=0; i<8; i++){
+            for(int j=0; j<8; j++){
+                if((j + i) % 2 == 0) picture[i][j] = PlaceColor.GREEN;
+                else           picture[i][j] = PlaceColor.LIME;
+            }
+        }
+        picture[2][1] = PlaceColor.BLACK;
+        picture[2][2] = PlaceColor.BLACK;
+        picture[2][5] = PlaceColor.BLACK;
+        picture[2][6] = PlaceColor.BLACK;
+        picture[3][1] = PlaceColor.BLACK;
+        picture[3][2] = PlaceColor.BLACK;
+        picture[3][5] = PlaceColor.BLACK;
+        picture[3][6] = PlaceColor.BLACK;
+        picture[4][3] = PlaceColor.BLACK;
+        picture[4][4] = PlaceColor.BLACK;
+        picture[5][2] = PlaceColor.BLACK;
+        picture[5][3] = PlaceColor.BLACK;
+        picture[5][4] = PlaceColor.BLACK;
+        picture[5][5] = PlaceColor.BLACK;
+        picture[6][2] = PlaceColor.BLACK;
+        picture[6][3] = PlaceColor.BLACK;
+        picture[6][4] = PlaceColor.BLACK;
+        picture[6][5] = PlaceColor.BLACK;
+        picture[7][2] = PlaceColor.BLACK;
+        picture[7][5] = PlaceColor.BLACK;
+
         List< String > args = super.getArguments();
 
         // Get host info from command line
@@ -71,20 +98,23 @@ public class CreeperBot extends ConsoleApplication implements Observer {
 
     private void run() {
         Scanner in = new Scanner(System.in);
-
-        Random random = new Random();
-
-        while (this.serverConn.game.isRunning()) {
-            PlaceColor color = PlaceColor.BLACK;
-            int c = random.nextInt(PlaceColor.values().length);
-            for(PlaceColor col : PlaceColor.values()){
-                if(c == col.getNumber()){
-                    color = col;
-                    //break;
+        System.out.println("Enter location for picture as: row col");
+        int row = in.nextInt();
+        int col = in.nextInt();
+        if(row + picture.length >= serverConn.game.getDim() || col + picture[0].length >= serverConn.game.getDim()){ row = 0; col = 0; }
+        if( row >= serverConn.game.getDim() || col >= serverConn.game.getDim()) {
+            System.out.println("Board to small for picture");
+            System.exit(0);
+        }
+        while (serverConn.game.isRunning()){
+            for (int i=row; i<row+picture.length; i++){
+                for(int j=col; j<col+picture[i].length; j++){
+                    serverConn.sendMove(i,j,picture[i-row][j-col]);
+                    try{Thread.sleep(500);}catch (InterruptedException e){}
                 }
             }
-            serverConn.sendMove(random.nextInt(serverConn.game.getDim()), random.nextInt(serverConn.game.getDim()), color);
         }
+
         System.out.println("Disconnected");
         System.exit(0);
     }
